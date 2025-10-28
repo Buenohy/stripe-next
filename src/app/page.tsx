6a4aa5900fc3b5467f20459fd1c66e3930f55d06
Gabriel Bueno Hygino <gabriel.buenohyginoc@gmail.com>
2025-10-28 16:41:41 -0300

    feat: CheckoutPage component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import CheckoutPage from '@/components/CheckoutPage';
-import convertToSubcurrency from '@lib/convertToSubcurrency';
+import convertToSubcurrency from '@/lib/convertToSubcurrency';
 import { Elements } from '@stripe/react-stripe-js';
 import { loadStripe } from '@stripe/stripe-js';
 
@@ -13,12 +13,23 @@ const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY);
 export default function Home() {
   const amount = 49.99;
   return (
-    <main className="max-w-6xl mx-auto p-10 text-white text-center border m-10 rounded-md bg-gradient-to-r from-blue-500 to-purple-500 ">
+    <main className="max-w-6xl mx-auto p-10 text-white text-center border m-10 rounded-md bg-linear-to-r from-blue-500 to-purple-500 ">
       <div className="mb-10">
         <h1 className="text-4xl font-extrabold mb-2">Sonny</h1>
         <h2 className="text-2xl">has requested</h2>
         <span className="font-bold">${amount}</span>
       </div>
+
+      <Elements
+        stripe={stripePromise}
+        options={{
+          mode: 'payment',
+          amount: convertToSubcurrency(amount),
+          currency: 'usd',
+        }}
+      >
+        <CheckoutPage amount={amount} />
+      </Elements>
     </main>
   );
 }
---
+++ src/components/CheckoutPage.tsx
@@ -0,0 +1,15 @@
+'use client';
+
+import { useElements, useStripe } from '@stripe/react-stripe-js';
+import { useState } from 'react';
+
+const CheckoutPage = ({ amount }: { amount: number }) => {
+  const stripe = useStripe();
+  const elements = useElements();
+
+  const [errorMensage, setErrorMessage] = useState<string>();
+  const [clientSecret, setClientSecret] = useState('');
+  const [loading, setLoading] = useState(false);
+};
+
+export default CheckoutPage;
