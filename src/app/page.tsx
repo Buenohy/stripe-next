8e353050e80b9fcd91dd592cd76caed5c369f37d
Gabriel Bueno Hygino <gabriel.buenohyginoc@gmail.com>
2025-10-28 16:17:39 -0300

    feat: .env

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,15 @@
+'use client';
+
+import CheckoutPage from '@/components/CheckoutPage';
+import convertToSubcurrency from '@lib/convertToSubcurrency';
+import { Elements } from '@stripe/react-stripe-js';
+import { loadStripe } from '@stripe/stripe-js';
+
+if (process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY === undefined) {
+  throw new Error('NEXT_PUBLIC_STRIPE_PUBLIC_KEY is not defined');
+}
+const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY);
+
 export default function Home() {
   return (
     <div>
