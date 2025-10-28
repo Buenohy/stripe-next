4f4d74730ff4543f6716ca1d42d906323bdb9c37
Gabriel Bueno Hygino <gabriel.buenohyginoc@gmail.com>
2025-10-28 16:24:45 -0300

    feat: inicial setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,9 +11,14 @@ if (process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY === undefined) {
 const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY);
 
 export default function Home() {
+  const amount = 49.99;
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
+    <main className="max-w-6xl mx-auto p-10 text-white text-center border m-10 rounded-md bg-gradient-to-r from-blue-500 to-purple-500 ">
+      <div className="mb-10">
+        <h1 className="text-4xl font-extrabold mb-2">Sonny</h1>
+        <h2 className="text-2xl">has requested</h2>
+        <span className="font-bold">${amount}</span>
+      </div>
+    </main>
   );
 }
