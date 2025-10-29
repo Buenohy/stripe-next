7d22b64ffd8b001d484cdd10241d0adff06f08f4
Gabriel Bueno Hygino <gabriel.buenohyginoc@gmail.com>
2025-10-28 22:59:20 -0300

    fix: error .env

--- src/app/api/create-payment-intent/route.ts
+++ src/app/api/create-payment-intent/route.ts
@@ -1,19 +1,20 @@
 import { NextRequest, NextResponse } from 'next/server';
-const stripe = require('stripe')(process.env.STRIPE_SCRET_KEY);
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 export async function POST(request: NextRequest) {
   try {
     const { amount } = await request.json();
 
-    const paymentIntent = await stripe.paymentIntent.create({
+    const paymentIntent = await stripe.paymentIntents.create({
       amount: amount,
       currency: 'usd',
       automatic_payment_methods: { enabled: true },
     });
 
-    return NextResponse.json({ clientSecret: paymentIntent.clientSecret });
+    return NextResponse.json({ clientSecret: paymentIntent.client_secret });
   } catch (error) {
-    // Handle other erros (e.g., network issues, parsing erros)
+    console.error('Internal Error:', error);
+    // Handle other errors (e.g., network issues, parsing errors)
     return NextResponse.json(
       { error: `Internal Server Error: ${error}` },
       { status: 500 }
